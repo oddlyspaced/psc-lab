001d5be72b66cbabb0dc797e819b4e2c8488c0a3
gamefreakoneone <amogh@outlook.com>
2019-10-19 07:48:15 +0530

    Take this fibonacci numbers!

---
+++ lab7/fibonumber/fibonacci.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int num,num1,num2,i,nxt;
+printf("how many terms do you want? \n");
+scanf("%d",&num);
+for(num1=0,num2=1,i=1;i<=num;i++)
+    {
+        printf("%d",num1);
+        nxt=num1+num2;
+        num1=num2;
+        num2=nxt;
+    }
+}
