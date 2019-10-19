43b486a836a85c43a13cbdb14ff621a48a8cd80b
gamefreakoneone <amogh@outlook.com>
2019-10-19 13:25:27 +0530

    Lab 6 problems

---
+++ lab6/reversi.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+int num,rnum;
+printf("Give me a number \n");
+    scanf("%d",&num);
+rnum=0;
+while (num != 0)
+  {
+    rnum = rnum * 10;
+    rnum = rnum + num%10;
+    num = num/10;
+  }
+
+printf("%d",rnum);
+return 0;
+}
