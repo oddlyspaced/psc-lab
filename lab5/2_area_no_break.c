c8316402f9395d92a9c05d0606b32a7c9a7f5907
oddlyspaced <oddlyspaced1@gmail.com>
2019-10-24 14:56:45 +0530

    Add lab 5 programs

---
+++ lab5/2_area_no_break.c
@@ -0,0 +1,24 @@
+// author - Hardik Srivastava (oddlyspaced)
+#include<stdio.h>
+void main() {
+    printf("Enter\n 1 for calculating area of circle\n 2 calculating for area of rectangle\n 3 for calculating area of triangle\n");
+    int radius, length, breadth, base, height;
+    int choice;
+    double area;
+    scanf(" %d", &choice);
+    switch(choice) {
+        case 1 : printf("Enter radius : \n");
+                 scanf("%d", &radius);
+                 area = 3.14 * radius * radius;
+                 printf("Area of circle : %lf", area);
+        case 2 : printf("Enter length and breadth : \n");
+                 scanf("%d %d", &length, &breadth);
+                 area = length * breadth;
+                 printf("Area of rectangle : %lf", area);
+        case 3 : printf("Enter base and height of triangle : \n");
+                 scanf("%d %d", &base, &height);
+                 area = 0.5 * base * height;
+                 printf("Area of triangle : %lf", area);
+        default : printf("Invalid entry!");
+    }
+}
